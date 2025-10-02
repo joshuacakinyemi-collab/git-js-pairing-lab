a4e25073b47d703f252335a48d497b8b2a9cbe2c
karizmagreen18 <Brooklynkay120@gmail.com>
2025-10-02 16:01:46 -0400

    completed the multiplesOfThree

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ const fiveToOneHundred = () => {
         }
 };
 
-fiveToOneHundred()
+//fiveToOneHundred();
+
+//Write a function multiplesOfThree that console logs all positive numbers to 100 that is a multiple of 3.
+const multiplesOfThree = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        }
+    }
+}
+
+multiplesOfThree();
