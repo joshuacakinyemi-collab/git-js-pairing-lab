767290a8718fa769c03cbc3bc0d6c6693e0b8bf7
Joshua Akinyemi <joshuacakinyemi@gmail.com>
2025-10-02 15:48:44 -0400

    solve numer1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //Code your solutions in this file
+const fiveToOneHundred = () => {
+    for (let i = 0; i <= 100; i++)
+        if (i >= 5 && i <= 100) {
+            console.log(i);
+        } else {
+            console.log('error');
+        }
+};
+
+fiveToOneHundred()
